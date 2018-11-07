c6d5187401d967095b16960809afa34b4e2bdcdb
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 18:26:01 +0100

    rust: Rename callback receivers.

--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -12,10 +12,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
                                           // Don't use device before ipcon is connected.
 
-    // Create receiver for humidity events.
-    let humidity_receiver = h.get_humidity_receiver();
+    let humidity_receiver = h.get_humidity_callback_receiver();
 
-    // Spawn thread to handle received events. This thread ends when the `h` object
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `h` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
         for humidity in humidity_receiver {
