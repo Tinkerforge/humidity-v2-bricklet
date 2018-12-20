ce47d98c858faf8a95345c44a7150db75a5a04c1
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:53:37 +0100

    rust: Reformat examples.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,8 +1,5 @@
-use std::{io, error::Error};
-use std::thread;
-use tinkerforge::{ip_connection::IpConnection, 
-                  humidity_v2_bricklet::*};
-
+use std::{error::Error, io, thread};
+use tinkerforge::{humidity_v2_bricklet::*, ip_connection::IpConnection};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,21 +10,21 @@ fn main() -> Result<(), Box<dyn Error>> {
     let h = HumidityV2Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-     let humidity_receiver = h.get_humidity_callback_receiver();
+    let humidity_receiver = h.get_humidity_callback_receiver();
 
-        // Spawn thread to handle received callback messages. 
-        // This thread ends when the `h` object
-        // is dropped, so there is no need for manual cleanup.
-        thread::spawn(move || {
-            for humidity in humidity_receiver {           
-                		println!("Humidity: {} %RH", humidity as f32 /100.0);
-            }
-        });
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `h` object
+    // is dropped, so there is no need for manual cleanup.
+    thread::spawn(move || {
+        for humidity in humidity_receiver {
+            println!("Humidity: {} %RH", humidity as f32 / 100.0);
+        }
+    });
 
-		// Set period for humidity callback to 1s (1000ms) without a threshold.
-		h.set_humidity_callback_configuration(1000, false, 'x', 0, 0);
+    // Set period for humidity callback to 1s (1000ms) without a threshold.
+    h.set_humidity_callback_configuration(1000, false, 'x', 0, 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -1,8 +1,5 @@
-use std::{io, error::Error};
-use std::thread;
-use tinkerforge::{ip_connection::IpConnection, 
-                  humidity_v2_bricklet::*};
-
+use std::{error::Error, io, thread};
+use tinkerforge::{humidity_v2_bricklet::*, ip_connection::IpConnection};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,23 +10,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     let h = HumidityV2Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-     let humidity_receiver = h.get_humidity_callback_receiver();
+    let humidity_receiver = h.get_humidity_callback_receiver();
 
-        // Spawn thread to handle received callback messages. 
-        // This thread ends when the `h` object
-        // is dropped, so there is no need for manual cleanup.
-        thread::spawn(move || {
-            for humidity in humidity_receiver {           
-                		println!("Humidity: {} %RH", humidity as f32 /100.0);
-		println!("Recommended humidity for human comfort is 30 to 60 %RH.");
-            }
-        });
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `h` object
+    // is dropped, so there is no need for manual cleanup.
+    thread::spawn(move || {
+        for humidity in humidity_receiver {
+            println!("Humidity: {} %RH", humidity as f32 / 100.0);
+            println!("Recommended humidity for human comfort is 30 to 60 %RH.");
+        }
+    });
 
-		// Configure threshold for humidity "outside of 30 to 60 %RH"
-		// with a debounce period of 10s (10000ms).
-		h.set_humidity_callback_configuration(10000, false, 'o', 30*100, 60*100);
+    // Configure threshold for humidity "outside of 30 to 60 %RH"
+    // with a debounce period of 10s (10000ms).
+    h.set_humidity_callback_configuration(10000, false, 'o', 30 * 100, 60 * 100);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
